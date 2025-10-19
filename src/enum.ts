21c5082e11357bdd7ccc2b4f2f2dc30f7b72ffb3
duyiliangfei <lf15835411402@163.com>
2025-10-19 18:55:46 +0800

    提交扑克牌枚举文件

---
+++ src/enum.ts
@@ -0,0 +1,23 @@
+enum Colors  {
+    Diamonds = '♦',
+    Clubs = '♣',
+    Hearts = '♥',
+    Spades = '♠'
+}
+
+enum Numbers {
+    Ace = 'A',
+    Two = '2',
+    Three = '3',
+    Four = '4',
+    Five = '5',
+    Six = '6',
+    Seven = '7',
+    Eight = '8',
+    Nine = '9',
+    Ten = '10',
+    Jack = 'J',
+    Queen = 'Q',
+    King = 'K',
+}
+
